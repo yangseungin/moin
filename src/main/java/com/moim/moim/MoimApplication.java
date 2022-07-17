90366e19775b7c5d0c4fe228eadab9ab989bd515
seungin yang <rhfpdk92@naver.com>
2022-07-17 23:33:21 +0900

    Initial commit

---
+++ src/main/java/com/moim/moim/MoimApplication.java
@@ -0,0 +1,13 @@
+package com.moim.moim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moim/moim/MoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moim.moim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
